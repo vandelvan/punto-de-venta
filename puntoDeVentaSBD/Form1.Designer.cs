0af7bbe4968084e868469bd3e26998f273b38eaa
Piloalucard <gustavo.padilla1054@alumnos.udg.mx>
2021-05-17 14:25:21 -0500

    Probando Npgsql

--- puntoDeVentaSBD/Form1.Designer.cs
+++ puntoDeVentaSBD/Form1.Designer.cs
@@ -24,16 +24,24 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label version;
 
         #endregion
     }
--- puntoDeVentaSBD/Form1.cs
+++ puntoDeVentaSBD/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace puntoDeVentaSBD
 {
@@ -15,6 +16,36 @@ namespace puntoDeVentaSBD
         public Form1()
         {
             InitializeComponent();
+            var cs = "Host=localhost;Username=postgres;Password=pswrd;Database=testdb";
+            try
+            {
+                using var con = new NpgsqlConnection(cs);
+                con.Open();
+                
+                using var cmd = new NpgsqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = "DROP TABLE IF EXISTS cars";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = @"CREATE TABLE cars(id SERIAL PRIMARY KEY, 
+                    name VARCHAR(255), price INT)";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Audi',52642)";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Mercedes',57127)";
+                cmd.ExecuteNonQuery();
+                
+            }
+            catch
+            {
+                Console.WriteLine("No se logro conectar a la database");
+            }
+
+            
+
         }
     }
 }
